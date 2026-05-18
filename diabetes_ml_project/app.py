08c9af0ef154dc50127d2b7f638f7108d3398e07
Vismay H <vismayvismay072@gmail.com>
2026-05-18 13:36:07 +0530

    connected model

--- diabetes_ml_project/app.py
+++ diabetes_ml_project/app.py
@@ -1,29 +1,55 @@
-from flask import Flask, render_template, request
-import pickle
+from flask import Flask, request, render_template_string
+import joblib
+import numpy as np
 
 app = Flask(__name__)
 
-model = pickle.load(open("diabetes_model.pkl","rb"))
+model = joblib.load("diabetes_ml_project/model/diabetes_model.pkl")
 
-@app.route("/")
+HTML = """
+<h1>GlucoNova Diabetes Risk Intelligence</h1>
+
+<form method="POST">
+Pregnancies: <input name="preg"><br><br>
+Glucose: <input name="glucose"><br><br>
+Blood Pressure: <input name="bp"><br><br>
+Skin Thickness: <input name="skin"><br><br>
+Insulin: <input name="insulin"><br><br>
+BMI: <input name="bmi"><br><br>
+Diabetes Pedigree Function: <input name="dpf"><br><br>
+Age: <input name="age"><br><br>
+
+<input type="submit" value="Predict">
+</form>
+
+<h2>{{result}}</h2>
+"""
+
+@app.route("/", methods=["GET","POST"])
 def home():
-    return render_template("index.html")
 
+    result=""
 
-@app.route("/predict", methods=["POST"])
-def predict():
+    if request.method=="POST":
+        data=np.array([[
+            float(request.form["preg"]),
+            float(request.form["glucose"]),
+            float(request.form["bp"]),
+            float(request.form["skin"]),
+            float(request.form["insulin"]),
+            float(request.form["bmi"]),
+            float(request.form["dpf"]),
+            float(request.form["age"])
+        ]])
 
-    prediction = model.predict([[1,148,72,35,0,33.6,0.627,50]])
+        prediction=model.predict(data)
 
-    if prediction[0]==1:
-        result="Diabetes Likely"
-    else:
-        result="No Diabetes"
+        if prediction[0]==1:
+            result="Diabetes likely"
+        else:
+            result="No diabetes detected"
 
-    return render_template(
-        "index.html",
-        prediction_text=result
-    )
+    return render_template_string(HTML,result=result)
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=5000)
