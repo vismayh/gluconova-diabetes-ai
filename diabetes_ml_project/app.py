7be02e0d709d3c4cd16c480ae125a5188e7b16df
Vismay H <vismayvismay072@gmail.com>
2026-05-18 12:59:12 +0530

    added website page

--- diabetes_ml_project/app.py
+++ diabetes_ml_project/app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "GlucoNova Diabetes Risk Intelligence is running!"
+    return """
+    <h1>GlucoNova Diabetes Risk Intelligence</h1>
+    <p>AI Diabetes Prediction System is running successfully</p>
+    <a href='/predict'>Go to Prediction</a>
+    """
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route("/predict")
+def predict():
+    return "Prediction page coming from your ML model"
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=5000)
