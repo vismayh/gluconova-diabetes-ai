dd4791b4f4f48597dc01af369e69d3f6d38797b5
Vismay H <vismayvismay072@gmail.com>
2026-05-18 12:40:25 +0530

    fixed flask name error

--- diabetes_ml_project/app.py
+++ diabetes_ml_project/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "GlucoNova Diabetes Risk Intelligence is running!"
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
